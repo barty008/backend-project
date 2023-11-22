813e2de6b06336edbe526f4fffcf30b1332b88be
barty008 <88056767+barty008@users.noreply.github.com>
2023-11-22 10:37:19 +0000

    Revert "Task five"

---
+++ __tests__/topic.test.js
@@ -0,0 +1,34 @@
+require("dotenv").config()
+const request = require("supertest")
+const { app } = require("../app")
+const seed = require("../db/seeds/seed")
+const db = require("../db/connection")
+const testData = require("../db/data/test-data")
+const fs = require("fs/promises")
+
+afterAll(() => {
+  return db.end()
+})
+
+beforeEach(() => {
+  return seed(testData)
+})
+
+describe("/api/topics", () => {
+  test("GET: 200 returns an array of all topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const { topics } = body
+
+        expect(topics.length).toBe(3)
+        topics.forEach((topic) => {
+          expect(topic).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String),
+          })
+        })
+      })
+  })
+})
