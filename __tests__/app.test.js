8c8304b1ba0a635bf38d216cff1d4297434de99a
barty008 <jamesb88@hotmail.co.uk>
2023-11-22 12:09:21 +0000

    jest sorted added

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -41,6 +41,7 @@ describe("/api/topics", () => {
       })
   })
 })
+const sorted = require("jest-sorted")
 describe("/api/articles", () => {
   test("GET: 200 gets all articles", () => {
     const { articleData: testArticleData } = testData
@@ -53,57 +54,69 @@ describe("/api/articles", () => {
         expect(articles.length).toBe(testArticleData.length)
       })
   })
-  test("GET: all articles in the correct order", () => {
-    const { articleData: testArticleData } = testData
-
-    return request(app)
-      .get("/api/articles")
-      .expect(200)
-      .then((response) => {
-        const { articles } = response.body
-        //  correct order logic
-        for (let i = 0; i < articles.length - 1; i++) {
-          // convert date strings into JS Date objects
-          const currentDate = new Date(articles[i].created_at).getTime()
-          const nextDate = new Date(articles[i + 1].created_at).getTime()
-          // compare the two dates as numbers using getTime()
-          expect(currentDate).toBeGreaterThanOrEqual(nextDate)
-        }
-      })
-  })
-  test("GET: articles with correct properties", () => {
+  test("GET: articles should be sorted by created_at in descending order", () => {
     return request(app)
       .get("/api/articles")
       .expect(200)
       .then((response) => {
         const { articles } = response.body
 
-        articles.forEach((article) => {
-          expect(article).toHaveProperty("author")
-          expect(article).toHaveProperty("title")
-          expect(article).toHaveProperty("article_id")
-          expect(article).toHaveProperty("topic")
-          expect(article).toHaveProperty("created_at")
-          expect(article).toHaveProperty("votes")
-          expect(article).toHaveProperty("article_img_url")
-          expect(article).toHaveProperty("comment_count")
-        })
+        const dateComparator = (a, b) =>
+          new Date(b.created_at) - new Date(a.created_at)
+        expect(articles).toBeSorted(dateComparator)
       })
   })
+  // test("GET: all articles in the correct order", () => {
+  //   const { articleData: testArticleData } = testData
 
-  test("GET: articles should not have a body property", () => {
-    return request(app)
-      .get("/api/articles")
-      .expect(200)
-      .then((response) => {
-        const { articles } = response.body
-
-        articles.forEach((article) => {
-          expect(article).not.toHaveProperty("body")
-        })
-      })
-  })
+  //   return request(app)
+  //     .get("/api/articles")
+  //     .expect(200)
+  //     .then((response) => {
+  //       const { articles } = response.body
+  //       //  correct order logic
+  //       for (let i = 0; i < articles.length - 1; i++) {
+  //         // convert date strings into JS Date objects
+  //         const currentDate = new Date(articles[i].created_at).getTime()
+  //         const nextDate = new Date(articles[i + 1].created_at).getTime()
+  //         // compare the two dates as numbers using getTime()
+  //         expect(currentDate).toBeGreaterThanOrEqual(nextDate)
+  //       }
+  //     })
 })
+test("GET: articles with correct properties", () => {
+  return request(app)
+    .get("/api/articles")
+    .expect(200)
+    .then((response) => {
+      const { articles } = response.body
+
+      articles.forEach((article) => {
+        expect(article).toHaveProperty("author")
+        expect(article).toHaveProperty("title")
+        expect(article).toHaveProperty("article_id")
+        expect(article).toHaveProperty("topic")
+        expect(article).toHaveProperty("created_at")
+        expect(article).toHaveProperty("votes")
+        expect(article).toHaveProperty("article_img_url")
+        expect(article).toHaveProperty("comment_count")
+      })
+    })
+})
+
+test("GET: articles should not have a body property", () => {
+  return request(app)
+    .get("/api/articles")
+    .expect(200)
+    .then((response) => {
+      const { articles } = response.body
+
+      articles.forEach((article) => {
+        expect(article).not.toHaveProperty("body")
+      })
+    })
+})
+
 // 1) check article properties have the properties:
 // 2) should not have a body property
 // send pr task 5 fixes
